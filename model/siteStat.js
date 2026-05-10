063f25f5103ed343f686e233145650952999bb41
vjhunjhun <jhunjhunwalavaibhav20@gmail.com>
2026-05-10 18:50:30 +0545

    schema constraints added for sitestats

--- model/siteStat.js
+++ model/siteStat.js
@@ -4,15 +4,18 @@ const Schema = mongoose.Schema;
 const statSchema = new Schema({
      studentJoined:{
         type:Number,
-        default:0
+        default: 0,
+        min:0
     },
     bookShared:{
         type:Number,
-        default:0
+        default: 0,
+        min:0
     },
     totalBooks:{
         type:Number,
-        default:0
+        default: 0,
+        min:0
     }
 });
 const SiteStat = mongoose.model("SiteStat",statSchema);
